2e2b5b865252ce08e9babcabc07b2b0fefd03da7
PranshavMI679 <pranshav.patel@mindinventory.com>
2026-07-31 19:57:45 +0530

    For Loop and Patterns

--- Learning/JS Learning-2/Fri_31-07-2026/patterns.js
+++ Learning/JS Learning-2/Fri_31-07-2026/patterns.js
@@ -5,6 +5,7 @@ for (let i = 1; i <= 5; i++){
     }
     console.log(text)
 }
+
 //result
 // *
 // **
@@ -20,3 +21,52 @@ for (let i = 1; i <= n; i++){
     }
     console.log(text)
 }
+
+//result
+// *****
+// ****
+// ***
+// **
+// *
+
+//--------------------
+
+let n = 5;
+for (let i = 1; i <= n; i++) {
+    let text = "";
+    for (let j = 1; j <= n - i; j++) {
+        text = text + " ";
+    }
+    for (let k = 1; k <= i; k++) {
+        text = text + "*";
+    }
+    console.log(text);
+}
+
+//result
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+//---------------
+
+let n = 5;
+for (let i = 1; i <= n; i++) {
+    let text = "";
+    for (let j = 1; j < i; j++) {
+        text = text + " ";
+    }
+    for (let k = 1; k <= n - i + 1; k++) {
+        text = text + "*";
+    }
+    console.log(text);
+}
+
+//result
+// *****
+//  ****
+//   ***
+//    **
+//     *
