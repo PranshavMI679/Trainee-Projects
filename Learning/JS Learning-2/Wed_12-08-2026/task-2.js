8e31c2e61b2300ea08155b346be4cbe21dc7e6ea
PranshavMI679 <pranshav.patel@mindinventory.com>
2026-08-13 19:18:11 +0530

    Thursday Patterns

--- Learning/JS Learning-2/Wed_12-08-2026/task-2.js
+++ Learning/JS Learning-2/Wed_12-08-2026/task-2.js
@@ -43,17 +43,22 @@ function pattern1(rows) {
 //     *
 
 //2
-function pattern2(rows){
-    for(let i=1; i<=rows; i++){
-        let text = ""
-        for(let j = 1; j<=i; j++){
-            text=  text + j
+function pattern2(rows){ 
+    for(let i=1; i<=rows; i++){ 
+        let text = "" 
+        for(let j = 1; j<=i; j++){ 
+            text = text + j 
+        } 
+        for(let s = 1; s <= (rows - i) * 2; s++){
+            text = text + " "
         }
-        
-        console.log(text)
-    }
-}
-pattern2(5)
+        for(let j = i; j >= 1; j--){
+          text = text + j
+        }
+        console.log(text) 
+    } 
+} 
+//pattern2(5)
 
 
 // Result
@@ -62,3 +67,69 @@ pattern2(5)
 // 123    321
 // 1234  4321
 // 1234554321
+
+//---extra
+function patternExtra(rows){ 
+    for(let i=1; i<=rows; i++){ 
+        let text = "" 
+        for(let j = 1; j<=i; j++){ 
+            text = text + j 
+        } 
+        for(let s = 1; s <= [(rows - i)*2]-1; s++){
+            text = text + " "
+        }
+        for(let j = i; j>=1; j--){
+            if(j==rows){
+            text = text
+          }
+            else{text = text + j}
+        }
+        console.log(text) 
+    } 
+} 
+//patternExtra(3)
+
+// Result
+// 1       1
+// 12     21
+// 123   321
+// 1234 4321
+// 123454321
+
+//3
+function pattern3(nums){
+  for(let i=1; i<=nums; i++){
+    let text = ""
+    for (let j=nums; j>=1; j--){
+     if(j<nums-i+1){
+      text = text + (nums-i+1)
+     }
+     else{
+      text = text + j
+     }
+    }
+    console.log(text)
+  }
+}
+pattern3(5)
+
+//Results
+// 5 5 5 5 5 
+// 5 4 4 4 4 
+// 5 4 3 3 3 
+// 5 4 3 2 2 
+// 5 4 3 2 1 
+
+//Results
+// 5 5 5 5 5 5 5 5 5 
+// 5 4 4 4 4 4 4 4 5 
+// 5 4 3 3 3 3 3 4 5 
+// 5 4 3 2 2 2 3 4 5 
+// 5 4 3 2 1 2 3 4 5 
+// 5 4 3 2 2 2 3 4 5 
+// 5 4 3 3 3 3 3 4 5 
+// 5 4 4 4 4 4 4 4 5 
+// 5 5 5 5 5 5 5 5 5
+
+
+
