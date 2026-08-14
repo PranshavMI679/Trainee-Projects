86701948f1ef03d821a9c1ed82af54cacf149b3e
PranshavMI679 <pranshav.patel@mindinventory.com>
2026-08-14 19:30:28 +0530

    Firday Patterns

--- Learning/JS Learning-2/Wed_12-08-2026/task-2.js
+++ Learning/JS Learning-2/Wed_12-08-2026/task-2.js
@@ -96,22 +96,22 @@ function patternExtra(rows){
 // 1234 4321
 // 123454321
 
-//3
-function pattern3(nums){
+//extra
+function pattern3extra(nums){
   for(let i=1; i<=nums; i++){
     let text = ""
     for (let j=nums; j>=1; j--){
      if(j<nums-i+1){
-      text = text + (nums-i+1)
+      text = text + (nums-i+1) + " "
      }
      else{
-      text = text + j
+      text = text + j + " "
      }
     }
     console.log(text)
   }
 }
-pattern3(5)
+//pattern3extra(5)
 
 //Results
 // 5 5 5 5 5 
@@ -119,6 +119,58 @@ pattern3(5)
 // 5 4 3 3 3 
 // 5 4 3 2 2 
 // 5 4 3 2 1 
+
+//extra
+function pattern3Extra(nums){
+  for(let i=1; i<=nums; i++){
+    let text = ""
+    for (let j=1; j<=nums; j++){
+     if(j<i){
+      text = text + j + " "
+     }
+     else{
+      text = text + i + " "
+     }
+    }
+    console.log(text)
+  }
+}
+//pattern3Extra(5)
+
+//REsult
+//11111
+//12222
+//12333
+//12344
+//12345
+
+//3
+function pattern3(nums) {
+  let value = nums * 2 - 1;
+
+  for (let i = 1; i <= value; i++) {
+    let text = "";
+    let row = i;
+    if (i > nums) {
+      row = value - i + 1;
+    }
+
+    for (let j = 1; j <= value; j++) {
+      let col = nums - j + 1;
+      if (j > nums) {
+        col = j - nums + 1;
+      }
+
+      if (col < nums - row + 1) {
+        text = text + (nums - row + 1) + " ";
+      } else {
+        text = text + col + " ";
+      }
+    }
+    console.log(text);
+  }
+}
+//pattern3(3);
 
 //Results
 // 5 5 5 5 5 5 5 5 5 
@@ -132,4 +184,38 @@ pattern3(5)
 // 5 5 5 5 5 5 5 5 5
 
 
+//4
+function pattern4(rows){
+  let value = (rows**2)
+  let i = 1
+  let text = ""
+  while(i<=rows){
+    let num = i.toString()
+    let numpad = num.padStart(3, "0") 
+    //text = text + numpad + " "
+    i++
+    if(i>=rows){
+      text = text + numpad + " "
+    }
+    else{text = text + (numpad) + " "}
+    console.log(text)
+  }
+}
+pattern4(3)
 
+//result
+// 001 002 003
+// 008 009 004
+// 007 006 005
+
+//result
+// 001  002  003  004  005  006  007  008  009  010
+// 036  037  038  039  040  041  042  043  044  011
+// 035  064  065  066  067  068  069  070  045  012
+// 034  063  084  085  086  087  088  071  046  013
+// 033  062  083  096  097  098  089  072  047  014
+// 032  061  082  095  100  099  090  073  048  015
+// 031  060  081  094  093  092  091  074  049  016
+// 030  059  080  079  078  077  076  075  050  017
+// 029  058  057  056  055  054  053  052  051  018
+// 028  027  026  025  024  023  022  021  020  019
