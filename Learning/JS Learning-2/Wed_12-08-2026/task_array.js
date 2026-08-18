46a7251016979e0936850749ec429839f8ac75fc
PranshavMI679 <pranshav.patel@mindinventory.com>
2026-08-18 19:13:26 +0530

    Tuesday Task

---
+++ Learning/JS Learning-2/Wed_12-08-2026/task_array.js
@@ -0,0 +1,38 @@
+// Solve the 4 problems using predefined methods and also by not using it - 2 times
+// When solving using predefined methods and functios - use only and only Array methods strictly.
+// Array methods which should not be used - concat(), filter(), find(), forEach(), includes(), slice(), map()
+// not even regexp or any thing else - just pure array related tasks/questions.
+
+arr = [
+	"Proin sed nisl vel turpis elementum facilisis vel ut metus.",
+	"Nam venenatis arcu et maximus pulvinar.",
+	"Vivamus at diam pellentesque ex tempus porta.",
+	"Cras imperdiet tortor euismod, sollicitudin lorem ut, accumsan ipsum.",
+	"Cras vestibulum arcu a magna ornare, non bibendum purus ultricies."
+]
+
+arr1 = [
+	"Proin et tortor molestie est pretium congue sit amet ut tortor.",
+	"Donec consectetur neque ac purus posuere dapibus.",
+	"Morbi congue nibh in ipsum lobortis ullamcorper.",
+	"Nam lacinia metus ac nisi lacinia, non sollicitudin nisl sodales.",
+	"Cras vestibulum arcu a magna ornare, non bibendum purus ultricies.",
+	"Nam venenatis arcu et maximus pulvinar."
+]
+
+
+//1 - Get diff of "arr" & "arr1" [ which strings are not the same "print that strings" ].
+
+
+
+//2 - Get count of words in every string of "arr"
+
+
+
+//3 - Get the string from an array which has given word or words. [ print that string & in both array with array name ]
+
+
+
+//4 - Get the same string from both arrays.
+
+
--- Learning/JS Learning-2/Wed_12-08-2026/task_string.js
+++ Learning/JS Learning-2/Wed_12-08-2026/task_string.js
@@ -2,6 +2,7 @@
 // When solving using predefined methods and functios - use only and only String methods strictly.
 // String methods which only return string or single value - no array no object allowed as result (split, match, matchAll)
 // not even regexp or any thing else - just pure string related tasks/questions.
+// you can use only arrays not array methods for normal solutions.
 
 str = `Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
     Vestibulum id turpis vehicula magna interdum venenatis. Sed pulvinar laoreet turpis quis cursus. 
@@ -10,11 +11,13 @@ str = `Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere 
     Phasellus vitae egestas tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
     Nulla et massa quam. Nulla ornare est non magna pulvinar convallis.`
 
-str1 = `Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.`
+str1 = `Class aptent taciti sociosqu ad Trainee litora Probation torquent per Trainee conubia nostra, Probation per inceptos himenaeos.`
 
 //1 - "str" word count.
+//Method
 //console.log(str.length)
 
+//Normal
 function question1(str){
     let len = str.length
     return len
@@ -23,53 +26,157 @@ console.log(question1(str))
 
 
 //2 - Merge "str" & "str1".
+//method
 let text = str.concat(str1)
 //console.log(text)
 
+//normal
 function add(a, b){
     return a + b
 }
 //console.log(add(str, str1))
 
 
-//3 - Get all the same words from the merge of "str" & "str1".(find the words which are in str1 from str)
-let words = str.search(str1)
-//console.log(words)
+//3 - Get all the same words from the merge of "str" & "str1".(find the words which are in str1 from str)]
+// Normal
+// function normalQuestion3(str, str1){
+//     let result = ""
+//     let word = ""
 
-function compare(a, b){
-    return a == b 
+//     for(let i=0; i<=str1.length; i++){
+
+//     }
+// }
+// normalQuestion3(str, str1)
+
+//Method
+function question3(str, str1) {
+    let result = "";
+    let word = "";
+
+    for (let i = 0; i <= str1.length; i++) {
+        let char = str1.charAt(i);
+        if (char === ' ' || i === str1.length) {
+            if (word !== "" && str.indexOf(word) !== -1) {
+                result = result.concat(word, " ");
+            }
+            word = "";
+        } else {
+            word = word + char;
+        }
+    }
+    return result;
 }
-//console.log(compare(str, str1))
+//console.log(question3(str, str1))
 
 
 //4 - Find position of given word in str or str1 in which string 
-console.log(str1.search("Class"))
-
-
 //5 - String has given word or not. if it has show its position.
-function pos(a, b){
-    if (a.includes(b) === true){
-        console.log(a.indexOf(b))
+//Normal
+function normalQuestion4and5(text, word) {
+    for (let i = 0; i < text.length; i++) {
+        let match = true;
+        for (let j = 0; j < word.length; j++) {
+            if (text[i + j] !== word[j]) {
+                match = false;
+            }
+        }
+        if (match === true) {
+            return "Found at position " + i;
+        }
     }
+    return "Not found";
 }
-//pos(str1, "Class")
+normalQuestion4and5(text, "Class")
+
+
+//method
+function question4and5(text, word) {
+    let pos = text.indexOf(word);
+    if (pos !== -1) {
+        return "Found at position " + pos;
+    }
+    return "Not found";
+}
+//console.log(question4and5(text, "patel"))
 
 
 //6 - Find All unique words from “str1” in comparison of “str”.
-let bool = str.includes(str1)
+//Method
+function question6(str, str1) {
+    let result = "";
+    let word = "";
 
-function compare(c){
-    if(c === true){
-        console.log(str.indexOf(str1))
+    for (let i = 0; i <= str1.length; i++) {
+        let char = str1.charAt(i);
+        if (char === ' ' || i === str1.length) {
+            if (word !== "" && str.indexOf(word) === -1) {
+                result = result.concat(word, " ");
+            }
+            word = "";
+        } else {
+            word = word + char;
+        }
     }
-    else {
-        console.log("not unique words found")
-    }
+    return result;
 }
-//compare(bool)
+const string = question6(str, str1)
+console.log(string)
 
-//7 - List all words & how many times it’s used.
-let n = str1.length
-for(let i = 0; i<n; i++){
-    //console.log(str1[i])
+//7 - List all unique words & how many times it’s used.
+// result string which we gets from ques-6 - use that and solve this 
+// Dont change the code - correct it
+
+//method
+
+function question7(uni) {
+    let total = 0;
+    let word = "";
+    let result = "";
+
+    for (let i = 0; i <= uni.length; i++) {
+        if (uni[i] !== " " && i !== uni.length) {
+            word = word + uni[i];
+        } 
+        else if (word !== "") {
+            if (result.indexOf(word) === -1) {
+                total = 0;
+                let position = uni.indexOf(word);
+             while (position !== -1) {
+                total = total + 1;
+                position = uni.indexOf(word, position + word.length);
+    }
+    result = result + word + " = " + total + "\n";
+ }
+    word = "";
 }
+}
+    return result;
+}
+console.log(question7(string));
+
+
+// function question7(uni){
+// let total = 0
+// let word = ""
+
+// for(let i = 0; i<=uni.length; i++){
+//     let char = uni.charAt(i)
+//    if (char === ' ' || i === uni.length) {
+//     if (word !== "") {
+//         total = 1; 
+//         console.log(`${word} = ${total}`);
+//         }
+//     word = ""; 
+//     }
+//     else {
+//         word = word + char
+//     }
+//     for(let value of string){
+//         let wordCount = 0
+
+//     }
+// }
+// }
+// console.log(question7(string))
+
